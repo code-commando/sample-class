86221016a37795b1563d4c11efb8e2ccdb9b1a21
MR <madhu.rebbana@gmail.com>
2018-07-17 20:29:28 -0700

    commit message

---
+++ 01-node-ecosystem/Jul18201803:29:9rk88wwvak.js
@@ -0,0 +1,12 @@
+12*2.monaco-list.list_id_1:focus .monaco-list-row.focused { background-color: #dcebfc; }
+.monaco-list.list_id_1:focus .monaco-list-row.focused:hover { background-color: #dcebfc; }
+.monaco-list.list_id_1:focus .monaco-list-row.selected { background-color: #52a8ff; }
+.monaco-list.list_id_1:focus .monaco-list-row.selected:hover { background-color: #52a8ff; }
+.monaco-list.list_id_1:focus .monaco-list-row.selected { color: #ffffff; }
+.monaco-list.list_id_1:focus .monaco-list-row.selected.focused { background-color: #3399ff; }
+.monaco-list.list_id_1:focus .monaco-list-row.selected.focused { color: #ffffff; }
+.monaco-list.list_id_1 .monaco-list-row.focused { background-color:  #dcebfc; }
+.monaco-list.list_id_1 .monaco-list-row.focused:hover { background-color:  #dcebfc; }
+.monaco-list.list_id_1 .monaco-list-row.selected { background-color:  #cccedb; }
+.monaco-list.list_id_1 .monaco-list-row.selected:hover { background-color:  #cccedb; }
+.monaco-list.list_id_1 .monaco-list-row:hover { background-color:  #f0f0f0; }
